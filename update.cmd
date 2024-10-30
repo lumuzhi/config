4dd9afb4073921b17794df5b553469794afcc6db
linwanrong <85693318+lumuzhi@users.noreply.github.com>
2024-10-30 16:20:14 +0800

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -3,14 +3,14 @@ setlocal enabledelayedexpansion
 :: 设置代码页为UTF (65001)
 chcp 65001 > nul
 color 02
-
+set "configFile=.ini"
 
 set "proxy=https://ghp.ci/"
 set "version_url=https://raw.githubusercontent.com/lumuzhi/config/main/version"
 set "update_url=https://raw.githubusercontent.com/lumuzhi/config/main/singbox.cmd"
 set "unix2dos_url=https://github.com/lumuzhi/config/blob/main/unix2dos.exe"
 
-for /f "tokens=1,2 delims==" %%a in (config) do (
+for /f "tokens=1,2 delims==" %%a in (%configFile%) do (
     if "%%a"=="version" set "version=%%b"
     if "%%a"=="country" set "country=%%b"
 )
@@ -23,22 +23,21 @@ if "!country!"=="CN" (
 )
 
 for /f "delims=" %%a in ('powershell -command "Invoke-RestMethod -Uri '!version_url!'"') do set "remoteVersion=%%a"
-
 if not exist unix2dos.exe (
     powershell -NoProfile -Command "Try { Invoke-RestMethod -Uri '%unix2dos_url%' -OutFile 'unix2dos.exe' } Catch { Write-Host 'unix2dos下载失败: $_' }"
 )
 curl -sL %update_url% > singbox.cmd
 unix2dos.exe singbox.cmd singbox.cmd
 
-for /f "tokens=1,* delims==" %%a in (config) do (
+for /f "tokens=1,* delims==" %%a in (%configFile%) do (
     if "%%a"=="version" (
         echo version=!remoteVersion!>temp
     ) else (
         echo %%a=%%b>>temp
     )
 )
-del config
-ren temp config
+del %configFile%
+ren temp %configFile%
 start "" "singbox.cmd"
 
 exit
